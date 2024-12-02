27a9285a1ab204200125dd557a1311d5f1184a03
suman-chandra-barman <sumanchandrabarman999@gmail.com>
2024-12-02 14:17:12 +0600

    [Add] Searching algorithm visualizer project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LinearSearch from "./components/LinearSearch.jsx";
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <h1>Searching algorithm visualizer</h1>
+        <LinearSearch/>
+    </div>
+  )
+}
+
+export default App
